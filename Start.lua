10cba5747505d40548e0d53fe11471d5348c5c0b
Luis3680 <itsluis3680@gmail.com>
2026-07-08 13:10:55 -0600

    e

--- Start.lua
+++ Start.lua
@@ -49,7 +49,6 @@ local LoadingScreen = _G.LoadingScreen
 
 
 loadScript("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/Info.lua")
----loadScript("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu.lua")
 
 local scripts = {
 	"https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/Scripts/MetalLastLife.lua",
