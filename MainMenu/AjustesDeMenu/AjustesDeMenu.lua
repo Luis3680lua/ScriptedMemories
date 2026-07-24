a3a4b4074c52d4746940156f3bd539376e13b805
Luis3680 <itsluis3680@gmail.com>
2026-07-24 15:39:46 -0600

    jaja

--- MainMenu/AjustesDeMenu/AjustesDeMenu.lua
+++ MainMenu/AjustesDeMenu/AjustesDeMenu.lua
@@ -4,11 +4,13 @@ if not Menu then return end
 local page = Menu:RegisterPage("Ajustes del Menú", "⚙️")
 page.Frame.AutomaticSize = Enum.AutomaticSize.Y
 
-Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/SelectorTema.lua")
+--- Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/SelectorTema.lua")
+
 Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/AccesoDirectoTeclado.lua")
 Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/AccesoDirectoControl.lua")
-Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/Default.lua")
-Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/LimpiarCache.lua")
+
+--- Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/Default.lua")
+--- Menu:LoadRemoteModule("https://raw.githubusercontent.com/Luis3680lua/ScriptedMemories/main/MainMenu/AjustesDeMenu/LimpiarCache.lua")
 
 task.wait(0.1)
 if Menu.UpdateCanvas then
